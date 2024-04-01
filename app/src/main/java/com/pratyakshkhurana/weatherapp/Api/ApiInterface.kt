8aaeacc689634514428240fb832369557305dc60
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2024-04-01 14:32:58 +0530

    Fix include
    - Fix base URL, and object response.

--- app/src/main/java/com/pratyakshkhurana/weatherapp/Api/ApiInterface.kt
+++ app/src/main/java/com/pratyakshkhurana/weatherapp/Api/ApiInterface.kt
@@ -1,14 +1,14 @@
 package com.pratyakshkhurana.weatherapp.Api
 
 import com.pratyakshkhurana.weatherapp.DataClass.CurrentWeather
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
-    @GET("/weather")
-    fun getCurrentWeather(
+    @GET("/data/2.5/weather")
+    suspend fun getCurrentWeather(
         @Query("q") q: String,
         @Query("appId") appID: String,
-    ): Call<CurrentWeather>
+    ): Response<CurrentWeather>
 }
--- app/src/main/java/com/pratyakshkhurana/weatherapp/Api/RetrofitInstance.kt
+++ app/src/main/java/com/pratyakshkhurana/weatherapp/Api/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    private const val BASE_URL = "https://api.openweathermap.org/"
     val api: ApiInterface by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
